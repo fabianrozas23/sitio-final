d8ad2e5127937f539ff92ca7f6c7627310e82c34
FelipeRojas1998 <felipe.rojasg@alumnos.uv.cl>
2020-11-15 12:36:40 -0300

    Actualizacion

--- src/App.js
+++ src/App.js
@@ -1,40 +1,59 @@
 import 'semantic-ui-css/semantic.min.css'
-import { Button, Icon, Label, Menu} from 'semantic-ui-react'
+import { Button, Icon, Label, Menu, Grid, Image, Segment, Header } from 'semantic-ui-react'
 import MyModal from './MyModal';
 import React from "react";
+
+
 
 
 function App() {
 
   var [openModal, setOpenModal] = React.useState(false);
-
-
+  
+  
   console.log("modal state", openModal);
   
   return (
     <div className="App">
        <Menu>
         <Menu.Item
-          name='editorials'          
+          name='Home'         
           onClick={()=>console.log("click menu 1")}
         >
-          Editorials
+          Home 
         </Menu.Item>
 
         <Menu.Item
-          name='reviews'
-          
-          onClick={()=>console.log("click menu 2")}
+          name='Formulario' 
+          //onClick={()=>console.log("click menu 2")}
+          onClick={(e,d)=>setOpenModal(true)}
         >
-          Reviews
+          Formulario
+          
         </Menu.Item>
 
         <Menu.Item
-          name='upcomingEvents'          
+          name='Tools'          
           onClick={()=>console.log("click menu 3")}
         >
-          Upcoming Events
+          Tools
         </Menu.Item>
+        
+        <Menu.Item
+          name='Descricpción' 
+          
+          onClick={()=>console.log("click menu 4")}
+        >
+          Descripción
+        </Menu.Item>
+
+        <Menu.Item
+          name='Redes Sociales'         
+          onClick={()=>console.log("click menu 5")}
+        >
+          Redes Sociales
+        </Menu.Item>
+        
       </Menu>
        <Button as='div' labelPosition='right' onClick={(e,d)=>setOpenModal(true)}>
       <Button icon>
@@ -42,9 +61,11 @@ function App() {
         Like
       </Button>
       <Label as='a' basic pointing='left'>
-        2,048
+        1000000
       </Label>
     </Button>
+    
+    
     <Button as='div' labelPosition='left'>
       <Label as='a' basic pointing='right'>
         2,048
@@ -54,17 +75,65 @@ function App() {
         Like
       </Button>
     </Button>
+    
+    
     <Button as='div' labelPosition='left'>
       <Label as='a' basic>
-        2,048
+        4800
       </Label>
       <Button icon>
         <Icon name='fork' />
       </Button>
     </Button>
     <MyModal visible={openModal} onClose={()=>{console.log("close"); setOpenModal(false);}} />
+
+
+    <Grid columns={2}>
+    <Grid.Column>
+      <Segment raised>
+        <Label as='a' color='red' ribbon>
+          Overview
+        </Label>
+        <span>Account Details</span>
+
+        <Image src='https://react.semantic-ui.com/logo.png' />
+
+        <Label as='a' color='blue' ribbon>
+          Community
+        </Label>
+        <span>User Reviews</span>
+
+        <Image src='https://react.semantic-ui.com/logo.png' />
+      </Segment>
+    </Grid.Column>
+
+    <Grid.Column>
+      <Segment>
+        <Label as='a' color='orange' ribbon='right'>
+          Specs
+        </Label>
+        <Image src='https://react.semantic-ui.com/logo.png' />
+
+        <Label as='a' color='teal' ribbon='right'>
+          Reviews
+        </Label>
+        <Image src='https://react.semantic-ui.com/logo.png' />
+      </Segment>
+    </Grid.Column>
+  </Grid>
+
+  <Header as='h2'>
+    <Icon.Group size='large'>
+      <Icon name='twitter' />
+      <Icon corner name='add' />
+    </Icon.Group>
+    Add on Twitter
+  </Header>
+
     </div>
   );
+
+
 }
 
 export default App;
--- src/MyModal.js
+++ src/MyModal.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Modal, Header, Icon, Button} from "semantic-ui-react"
+import {Modal, Header, Icon, Button, Form, Container} from "semantic-ui-react"
 
 export default function MyModal(props){
     let {visible, onClose} = props;
@@ -21,20 +21,51 @@ export default function MyModal(props){
           >
             <Header icon>
               <Icon name='archive' />
-              Archive Old Messages
+              Formulario
             </Header>
             <Modal.Content>
               <p>
-                    Modal para la clase de desarrollo web
+                    Formulario del Usuario 
               </p>
             </Modal.Content>
+
+            <Container>
+    <p>
+      Agregar datos personales del usuario para poder guardarlos.
+      
+      
+    </p>
+  </Container>
+
+  <Form>
+    <Form.Group unstackable widths={3}>
+      <Form.Input label='First name' placeholder='First name' />
+      <Form.Input label='Last name' placeholder='Last name' />
+      <Form.Input label='Age' placeholder='Age' />
+
+    </Form.Group>
+
+    <Form.Group widths={3}>
+      <Form.Input label='Address' placeholder='Address' />
+      <Form.Input label='Phone' placeholder='+569' />
+      <Form.Input label='Codigo Postal' placeholder='Codigo Postal' />
+    </Form.Group>
+    <Form.Checkbox label='I agree to the Terms and Conditions' />
+    <Button type='submit'>Submit</Button>
+     
+  
+  </Form>
+
+
+
             <Modal.Actions>
-              <Button basic color='red' inverted onClick={() =>onClose() }>
-                <Icon name='remove' /> No
+              <Button color='red' inverted onClick={() =>onClose() }>
+                <Icon name='remove' /> Cancelar
               </Button>
               <Button color='green' inverted onClick={() => onClose()}>
-                <Icon name='checkmark' /> Yes
+                <Icon name='checkmark' /> Aceptar
               </Button>
+              
             </Modal.Actions>
           </Modal>)
     } else {
