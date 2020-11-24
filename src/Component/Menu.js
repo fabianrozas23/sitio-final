f20269a5a5bab0854d6fdfd21d2c25f0fb586fb7
FelipeRojas1998 <felipe.rojasg@alumnos.uv.cl>
2020-11-23 23:57:12 -0300

    Hola3

--- src/Component/Menu.js
+++ src/Component/Menu.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import { Menu } from 'semantic-ui-react'
+import { Menu, Dropdown } from 'semantic-ui-react'
 
-const colorsA = ['Home', 'Concurso', 'Yellow', 'Olive', 'Green', 'Teal']
+const colorsA = ['Home', 'Concurso','Ideas', 'Información', 'Contacto', 'Herramientas']
 
 export default class MenuExampleColoredInverted extends Component {
   state = { activeA: colorsA[0] }
@@ -24,6 +24,18 @@ export default class MenuExampleColoredInverted extends Component {
               onClick={this.handleAClick}
             />
           ))}
+          <Dropdown item text='Redes Sociales'>
+          <Dropdown.Menu>
+            
+            <Dropdown.Header></Dropdown.Header>
+            <Dropdown.Item>Facebook</Dropdown.Item>
+            <Dropdown.Item>Instagram</Dropdown.Item>
+            <Dropdown.Item>Google</Dropdown.Item>
+            <Dropdown.Item>Twitter</Dropdown.Item>
+            
+      
+          </Dropdown.Menu>
+        </Dropdown>
         </Menu>
 
         
